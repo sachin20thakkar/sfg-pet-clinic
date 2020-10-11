9b2cd127ed5c0e6977f5292feed65346318a4452
Sachin Thakkar <sachin20thakkar>
2020-10-11 08:50:54 +0530

    Implement Spring Conf for Services - Closes #21

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -8,13 +8,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class DataLoader implements CommandLineRunner {
     private final OwnerServiceMap ownerServiceMap;
     private final VetServiceMap vetServiceMap;
 
-    public DataLoader() {
-        this.ownerServiceMap = new OwnerServiceMap();
-        this.vetServiceMap = new VetServiceMap();
+    public DataLoader(OwnerServiceMap ownerServiceMap, VetServiceMap vetServiceMap) {
+        this.ownerServiceMap = ownerServiceMap;
+        this.vetServiceMap = vetServiceMap;
     }
 
     @Override
